002de747c4407bb563b36045b19079bfe0d851a8
Dharshini11S <dharshinisuba1106@gmail.com>
2025-12-11 20:16:59 +0530

    Add test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+console.log("Running demo test for Music Streaming App...");
+const expected = "ok";
+if (expected !== "ok") {
+  console.error("Test failed");
+  process.exit(1);
+}
+console.log("Demo test passed");
